667512680b075321a6563b6034cc4f4fd2ff05e4
perezandr <andrea.perez.pa@gmail.com>
2024-02-21 18:12:24 +0100

    Fix: modified path for the skills icons

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,35 +4,35 @@ const Skills = () => {
   return (
     <section className={styles.SkillsContainer}>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/html.svg" alt="Html5" />
+        <img src="/icons/html.svg" alt="Html5" />
         <p>Html5</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/css.svg" alt="CSS3" />
+        <img src="/icons/css.svg" alt="CSS3" />
         <p>Css3</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/javascript.svg" alt="Javascript" />
+        <img src="/icons/javascript.svg" alt="Javascript" />
         <p>Javascript</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/react.svg" alt="ReactJS" />
+        <img src="/icons/react.svg" alt="ReactJS" />
         <p>React</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/sass.svg" alt="Sass" />
+        <img src="/icons/sass.svg" alt="Sass" />
         <p>Sass</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/nextjs.svg" alt="NextJS" />
+        <img src="/icons/nextjs.svg" alt="NextJS" />
         <p>Next.js</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/vitejs.svg" alt="Vite" />
+        <img src="/icons/vitejs.svg" alt="Vite" />
         <p>Vite</p>
       </div>
       <div className={styles.ImageContainer}>
-        <img src="./public/icons/threejs.svg" alt="ThreeJS" />
+        <img src="/icons/threejs.svg" alt="ThreeJS" />
         <p>Three.js</p>
       </div>
     </section>
